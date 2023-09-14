e635bb81ac5c672b290d4e35379ca77eab92b566
MonnNster4 <monsantiago09@gmail.com>
2023-09-15 04:06:13 +0800

    updt

--- assets/js/dataTables/jquery.dataTables.js
+++ assets/js/dataTables/jquery.dataTables.js
@@ -10606,25 +10606,7 @@
 			"sInfo": "Showing _START_ to _END_ of _TOTAL_ entries",
 	
 	
-			/**
-			 * Display information string for when the table is empty. Typically the
-			 * format of this string should match `info`.
-			 *  @type string
-			 *  @default Showing 0 to 0 of 0 entries
-			 *
-			 *  @dtopt Language
-			 *  @name DataTable.defaults.language.infoEmpty
-			 *
-			 *  @example
-			 *    $(document).ready( function() {
-			 *      $('#example').dataTable( {
-			 *        "language": {
-			 *          "infoEmpty": "No entries to show"
-			 *        }
-			 *      } );
-			 *    } );
-			 */
-			"sInfoEmpty": "Showing 0 to 0 of 0 entries",
+		
 	
 	
 			/**
